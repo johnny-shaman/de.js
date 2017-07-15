9b2c8e51b28f32a9a7c58108cf329346c64c2877
johnny-shaman <johnny_shaman@ae.auone-net.jp>
2017-07-15 20:17:47 +0900

    Update buster.js

--- buster.js
+++ buster.js
@@ -3,10 +3,9 @@ var config = module.exports;
 config["My tests"] = {
     env: "browser",        // or "node"
     sources: [
-        "lib/mylib.js", // Paths are relative to config file
-        "src/**/*.js"   // Glob patterns supported
+        "./de.js", // Paths are relative to config file
     ],
     tests: [
         "test/*-test.js"
     ]
-}
+};
